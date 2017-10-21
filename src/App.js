2266a174eb42fc89b3c8484b345762ee2169dae2
Brandon Rubenstein <brandon@brandonrubenstein.com>
2017-10-21 16:53:45 -0700

    Sets up starships component, displays list of ships

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
 import Starships from './components/Starships';
 
-//styles
 import './App.scss';
+
 class App extends Component {
+	state = {
+		counter: 0,
+		loading: true
+	}
+
+
+	componentWillMount(){
+    
+	}
+
+	componentDidMount(){
+		const that = this;
+		setTimeout(function (){
+			that.setState({
+				loading: false
+			})
+		}, 1500)
+	}
+
+	componentWillReceiveProps(){
+
+	}
+
+	logHello = () => {
+		console.log("Hello world!");
+	}
+
+	multiply = () => {
+		this.setState({
+			counter: this.state.counter + 2,
+		})
+	}
+
+	clickHandler = () => {
+		this.logHello();
+		this.multiply();
+	}
+
   render() {
     return (
       <div>
-        <h1>Star Wars</h1>
+      {this.state.loading ? "Is loading" : "Loading complete" }
+        <h1 onClick={this.clickHandler}>Star Wars</h1>
+        <h2>{this.state.counter}</h2>
 
         <Starships />
       </div>
--- src/components/Starships/index.js
+++ src/components/Starships/index.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import {
-  fetchShips,
-} from './ducks.js';
+import swapiModule from '../swapi.js';
+
+const swapi = new swapiModule();
 
 class Starships extends React.Component {
-  componentDidMount() {
-    fetchShips();
+
+  state = {
+    ships: [],
+    currentSearch: ''
   }
+
+  componentDidMount() {
+    const that = this;
+    
+    swapi.getStarships(res => {
+      console.log(res);
+      that.setState({
+        ships: [...that.state.ships, ...res.results]
+      })
+    });
+
+  }
+
+  updateSearch = (e) => {
+    this.setState({
+      currentSearch: e.target.value
+    })
+  } 
+
   render() {
     return (
       <div>
         <h2>Starships</h2>
+        <input type="text" name="search" onChange={this.updateSearch}/>
+        <ul>
+          {
+            this.state.ships.map( (ship, index) => {
+               if ( ship.name.toLowerCase().indexOf(this.state.currentSearch.toLowerCase()) !== -1 ) {
+                 return <li key={`starship${index}`}>{ship.name}</li> 
+               }
+             })            
+          }
+          
+        </ul>
+        <button>Prev</button>
+        <button>Next</button>
       </div>
     );
   }
