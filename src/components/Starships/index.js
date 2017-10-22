8629b7854bd8d9b5b0ed16f04f21dba5ce2cb801
Brandon Rubenstein <brandon@brandonrubenstein.com>
2017-10-22 09:14:34 -0700

    Displays one page of starships, parses API URL for page parameter value

--- src/components/Starships/index.js
+++ src/components/Starships/index.js
@@ -7,18 +7,27 @@ class Starships extends React.Component {
 
   state = {
     ships: [],
+    page: 1,
     currentSearch: ''
   }
 
   componentDidMount() {
     const that = this;
-    
-    swapi.getStarships(res => {
-      console.log(res);
-      that.setState({
-        ships: [...that.state.ships, ...res.results]
-      })
-    });
+
+//    do {
+      swapi.getStarships(that.state.page, res => {                
+        var next = that.getUrlParams(res.next)['page'];      
+        that.setState({
+          page: next
+        })
+
+        that.setState({
+          ships: [...that.state.ships, ...res.results]
+        })
+
+      });
+//    } while ( that.state.page != null )
+
 
   }
 
@@ -26,7 +35,47 @@ class Starships extends React.Component {
     this.setState({
       currentSearch: e.target.value
     })
-  } 
+  }
+
+  getUrlParams = (url) => {
+    // https://www.sitepoint.com/get-url-parameters-with-javascript/
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    var obj = {};
+    if (queryString) {
+      queryString = queryString.split('#')[0];
+      var arr = queryString.split('&');
+
+      for (var i=0; i<arr.length; i++) {      
+        var a = arr[i].split('=');
+        var paramNum = undefined;
+        var paramName = a[0].replace(/\[\d*\]/, function(v) {
+          paramNum = v.slice(1,-1);
+          return '';
+        });
+
+        var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+        paramName = paramName.toLowerCase();
+        paramValue = paramValue.toLowerCase();
+      
+        if (obj[paramName]) {
+          if (typeof obj[paramName] === 'string') {
+            obj[paramName] = [obj[paramName]];
+          }
+          if (typeof paramNum === 'undefined') {          
+            obj[paramName].push(paramValue);
+          }
+          else {          
+            obj[paramName][paramNum] = paramValue;
+          }
+        }      
+        else {
+        obj[paramName] = paramValue;
+        }
+      }
+    }
+  return obj;
+}
 
   render() {
     return (
