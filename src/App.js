628633bbae2fe99b8a3af6308c459befff3bbd83
Cameron Zaas <cameronzaas@gmail.com>
2017-10-22 11:53:10 -0700

    Added react-router-dom and some basic routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
+
+import Home from './components/Home';
 import Starships from './components/Starships';
+import NotFound from './components/NotFound';
 
 //styles
 import './App.scss';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Star Wars</h1>
+      <Router>
+        <div>
+          <h1>Star Wars</h1>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/starships">Starships</Link></li>
+            <li><Link to="/some-broken-link">Broken Link</Link></li>
+          </ul>
 
-        <Starships />
-      </div>
+          <Switch>
+            <Route path="/" exact render={Home} />
+            <Route path="/starships" component={Starships} />
+            <Route path="*" render={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
