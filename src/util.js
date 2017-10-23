d4e78ef5ddbcc1774fa8e7ff31cd1c6f0163bf6a
Cameron Zaas <cameronzaas@gmail.com>
2017-10-22 19:27:21 -0700

    Removed warning from util file (function was being declared within loop).

--- src/util.js
+++ src/util.js
@@ -6,13 +6,14 @@ export function getUrlParams(url) {
     queryString = queryString.split('#')[0];
     var arr = queryString.split('&');
 
+    function paramNumSlicer(v) {
+      paramNum = v.slice(1,-1);
+      return '';
+    }
     for (var i=0; i<arr.length; i++) {      
       var a = arr[i].split('=');
       var paramNum = undefined;
-      var paramName = a[0].replace(/\[\d*\]/, function(v) {
-        paramNum = v.slice(1,-1);
-        return '';
-      });
+      var paramName = a[0].replace(/\[\d*\]/, paramNumSlicer);
 
       var paramValue = typeof(a[1])==='undefined' ? true : a[1];
 
