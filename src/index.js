6ed5d7509623de9e32f7f39d58bc931618c24d45
Cameron Zaas <cameronzaas@gmail.com>
2017-10-22 19:28:55 -0700

    - Removed unused css - Updated reset

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
